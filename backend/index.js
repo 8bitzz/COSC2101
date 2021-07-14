867fd12745f3600b8a3d81638fc4b907634ea4e4
Nghia Tran <vinhnghiatran@gmail.com>
2021-07-14 17:04:07 +0700

    Basic models, controller, routes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 4000
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var helmet = require("helmet");
+var cors = require("cors");
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Import routes
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// App config
+var app = express();
+var PORT = process.env.PORT;
+
+//MongoDB
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+//Middleware
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    console.log(req.url)
+    next();
+});
+
+// Routes
+
+// Default route
+app.get("/", function (req, res) {
+    res.send(`Restful API is running on port ${PORT}!`);
+});
+
+// Express App initialize
+app.listen(PORT, function () {
+    console.log(`Your server is running on port ${PORT}`);
+});
