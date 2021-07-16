a8ee784e1ee9997a3019b368ed2b804acdc3094a
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-07-16 10:47:19 +0700

    Login user API

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -15,7 +15,24 @@ const registerUser = async (req, res) => {
   }
 }
 
+const loginUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email});
+    !user && res.status(401).json("Can not find any user with that email address");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+    originalPassword !== req.body.password && res.status(401).json("Incorrect password");
+
+    // Extract password from response
+    const { password, ...userInfo} = user._doc;
+    res.status(200).json(userInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
 
 module.exports = {
-  registerUser
+  registerUser,
+  loginUser
 };
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
-const { registerUser } = require("../controllers/userControllers");
+const { registerUser, loginUser } = require("../controllers/userControllers");
 
 // REGISTER
 router.post("/register", registerUser);
+
+// LOGIN
+router.post("/login", loginUser);
 
 module.exports = router;
