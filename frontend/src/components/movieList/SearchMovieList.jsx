ad57854c09100b3c341abc62749088739fbeae48
unknown <s3757107@rmit.edu.vn>
2021-07-22 09:32:07 +0700

    Search bar implementation

---
+++ frontend/src/components/movieList/SearchMovieList.jsx
@@ -0,0 +1,13 @@
+import MovieList from "./MovieList"
+import { useState } from "react";
+
+const SearchMovieList = ({ search, movie }) => {
+    
+    return (
+        <div>
+            This is search movie page
+        </div>
+    )
+}
+
+export default SearchMovieList
