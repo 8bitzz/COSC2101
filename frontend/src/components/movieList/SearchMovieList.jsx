c8b8e2ba86deaf8295d6f0e046596a58325325bb
Long Tran <54924290+DanCat6300@users.noreply.github.com>
2021-07-23 09:53:52 +0700

    Implement filter by genres

--- frontend/src/components/movieList/SearchMovieList.jsx
+++ frontend/src/components/movieList/SearchMovieList.jsx
@@ -4,29 +4,29 @@ import axios from 'axios';
 import "./movieList.css";
 
 const SearchMovieList = ({ search }) => {
-    const [filteredmovies, setFilteredMovie] = useState([]);
-    useEffect(() => {
-        axios
-            .get(`http://localhost:4000/api/v1/movies?name=${search}`)
-            .then(res => {
-                console.log(res)
-                setFilteredMovie(res.data.data.movies)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        // eslint-disable-next-line
-    }, [search])
-    const listRef = useRef();
-    return (
-        <div className="w-full mt-10">
-            {(filteredmovies.length <= 0) ? (
-                <div className="text-white">
-                    <p >There is no matching movie</p>
-                </div>
-            ) : (
-                <div className="movieContainer ml-12 mt-3 flex flex-wrap" ref={listRef}>
-                    {/* {filteredmovies.map((movie, index) => {
+  const [filteredmovies, setFilteredMovie] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/movies?name=${search}`)
+      .then(res => {
+        console.log(res)
+        setFilteredMovie(res.data.data.movies)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    // eslint-disable-next-line
+  }, [search])
+  const listRef = useRef();
+  return (
+    <div className="w-full mt-10">
+      {(filteredmovies.length <= 0) ? (
+        <div className="text-white">
+          <p >There is no matching movie</p>
+        </div>
+      ) : (
+        <div className="movieContainer ml-12 mt-3 flex flex-wrap" ref={listRef}>
+          {/* {filteredmovies.map((movie, index) => {
                         return (
                             <div>
                                 <img
@@ -37,17 +37,17 @@ const SearchMovieList = ({ search }) => {
                             </div>
                         )
                     })} */}
-                    <div className="grid grid-cols-3 gap-4">
-                        {filteredmovies.map((movie, index) => {
-                            return (
-                                <MovieItem movie={movie} key={movie._id} index={index} />
-                            );
-                        })}
-                    </div>
-                </div>
-            )}
+          <div className="grid md:grid-cols-4 lg:grid-cols-6 gap-4">
+            {filteredmovies.map((movie, index) => {
+              return (
+                <MovieItem movie={movie} key={movie._id} index={index} />
+              );
+            })}
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default SearchMovieList
