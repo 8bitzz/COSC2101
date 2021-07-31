a258c1c4c0558d6dceb30529b88da3afb47d2279
unknown <s3757107@rmit.edu.vn>
2021-07-31 14:12:27 +0700

    Add UI deatil

--- frontend/src/components/movieList/Details.jsx
+++ frontend/src/components/movieList/Details.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./movieList.css";
-import "./details.css";
+import "./details.css"
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import { useHistory } from 'react-router-dom'
@@ -9,6 +9,9 @@ export default function Details(props) {
   const history = useHistory();
 
   const [movie, setMovie] = useState([]);
+  const [category, setCategory] = useState([]);
+  var gerne = '';
+
   useEffect(() => {
     const _id = props.match.params._id;
     console.log(_id)
@@ -22,36 +25,79 @@ export default function Details(props) {
       .catch(err => {
         console.log(err)
       })
-
   }, [])
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/categories`)
+      .then((res) => {
+        console.log(res)
+        setCategory(res.data.data.categories.map((item) => item))
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // eslint-disable-next-line
+  }, []);
+  let youtubeURL = movie.trailerURL
+
   return (
-    <div className="container h-screen w-full mt-40 mx-10 md:object-center">
+    <div className="h-screen w-full mt-40 mx-10 md:object-center">
       <div className="bg-netflix-black overflow-hidden text-white md:object-center">
-        <div className="movie-wrapper border">
-        <h1 className="mb-10 ml-12 text-3xl font-bold text-white">{movie.title}</h1>
-       <button onClick={() => history.goBack()} className="p-3 ml-10 border-2 bg-black border-white text-white">Back </button>
-          <div className="movie-img-wrapper">
-            <img src={movie.thumbnail} alt="No movie image found" className="md:object-center movie-img-wrapper__thumbnail-img" />
+        <div className="movie-wrapper">
+          <div className="trailer-wrapper">
+            <iframe width="420" height="315" frameBorder="0" allowFullScreen src={
+              (movie.trailerURL)?(movie.trailerURL.replace('/watch?v=','/embed/')) : ('')} frameborder="0"></iframe>
           </div>
           <div className="movie-details">
-            <div className="movie-details__item movie-details__item movie-details__desc">
-              {movie.description}
-            </div>
-            <div className="movie-details__item movie-details__release-date">
-              <b>Released Date</b>: {movie.publishYear}
-            </div>
-            <div className="movie-details__item movie-details__duration">
-              <b>Duration</b>: {movie.duration}
-            </div>
-            <div className="movie-details__item movie-details__gerne">
-              <b>Actor</b>: {movie.casts}
-            </div>
-            <div className="movie-details__item movie-details__price">
-              <b>Price</b>: {movie.price}
+            <div className="grid grid-cols-3 gap-4">
+              <div className="col-span-2">
+                <div className="movie-details__item movie-details__release-date font-extralight pb-3">
+                  {movie.publishYear}
+                </div>
+                <div className="movie-details__item movie-details__item movie-details__title font-bold text-4xl pb-3">
+                  <h1>{movie.title}</h1>
+                </div>
+                <div className="movie-details__item movie-details__item movie-details__desc">
+                  {movie.description}
+                </div>
+              </div>
+              <div>
+                <div className="movie-details__item movie-details__duration">
+                  <b className="font-light">Duration</b>: {movie.duration}
+                </div>
+                <div className="movie-details__item movie-details__casts">
+                  <b className="font-light">Casts</b>: {movie.casts}
+                </div>
+                <div className="movie-details__item movie-details__gerne">
+                  {category.map((category) => {
+                    {
+                      (movie.category === category._id) ? (
+                        <div>
+                          {gerne = category.name}
+                        </div>
+                      ) : (
+                        <div>
+                        </div>
+                      )
+                    }
+                  })}
+                  <b className="font-light">Gerne</b>: {gerne}
+                </div>
+                <div className="movie-details__item movie-details__price">
+                  <b className="font-light">Price</b>: {movie.price}
+                </div>
+              </div>
             </div>
           </div>
-          <button className="p-3 ml-10 border-2 bg-black border-white text-white">Add to cart</button>
+        </div>
+        <div className="flex flex-row">
+          <div>
+            <button onClick={() => history.goBack()} className="p-3 ml-10 border-2 bg-black border-white text-white">Back </button>
+          </div>
+          <div>
+            <button className="p-3 ml-10 border-2 bg-black border-white text-white">Add to cart</button>
+          </div>
         </div>
       </div>
     </div>
