90f50a92798612fab8114fc7547f269aa8d1adff
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-07-31 09:43:08 +0700

    Refractor SearchedMovie page

--- frontend/src/components/movieList/SearchMovieList.jsx
+++ frontend/src/components/movieList/SearchMovieList.jsx
@@ -1,46 +1,58 @@
 import { useEffect, useState, useRef } from "react";
 import MovieItem from "../movieItem/MovieItem";
-import axios from 'axios';
+import axios from "axios";
 import "./movieList.css";
+import { useLocation } from "react-router-dom";
+import NavBar from "../navbar/NavBar";
 
-const SearchMovieList = ({searchTerm}) => {
-  const [filteredmovies, setFilteredMovie] = useState([]);
-  let term = searchTerm;
-  const search = new URLSearchParams(term).get("term")
+const SearchMovieList = ({ searchTerm }) => {
+  // const [filteredmovies, setFilteredMovie] = useState([]);
+  // let term = searchTerm;
+  // const search = new URLSearchParams(term).get("term")
+
+  const param = useLocation().search;
+  const search = new URLSearchParams(param).get("term");
+  console.log(search);
+  const [filteredMovies, setFilteredMovies] = useState([]);
 
   useEffect(() => {
     axios
       .get(`http://localhost:4000/api/v1/movies?name=${search}`)
-      .then(res => {
-        console.log(res)
-        setFilteredMovie(res.data.data.movies)
+      .then((res) => {
+        console.log(res);
+        setFilteredMovies(res.data.data.movies);
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [search])
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [search]);
   const listRef = useRef();
   return (
-    <div className="container-fluid h-screen w-full mt-40 mx-10">
-     
-      {(filteredmovies.length <= 0) ? (
-        <div className="text-white">
-          <p>There is no matching movies with provided keyword</p>
-        </div>
-      ) : (
-        <div className="movieContainer ml-12 mt-3 flex flex-wrap" ref={listRef}>
-          <div className="grid md:grid-cols-4 lg:grid-cols-6 gap-4">
-            {filteredmovies.map((movie, index) => {
-              return (
-                <MovieItem movie={movie} key={movie.id} index={index}/>
-              )
-            })}
+    <div>
+      <NavBar />
+      <div className="container-fluid h-screen w-full mt-40 mx-10">
+        {filteredMovies.length <= 0 ? (
+          <div className="text-white">
+            <p>There is no matching movies with provided keyword</p>
           </div>
-        </div>
-      )}
+        ) : (
+          <div
+            className="movieContainer ml-12 mt-3 flex flex-col flex-wrap"
+            ref={listRef}
+          >
+            <div className="text-white text-2xl mb-10">
+              <p>Result for: "{search}" </p>
+            </div>
+            <div className="grid md:grid-cols-4 lg:grid-cols-6 gap-4 ">
+              {filteredMovies.map((movie, index) => {
+                return <MovieItem movie={movie} key={movie.id} index={index} />;
+              })}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
-
-export default SearchMovieList
+export default SearchMovieList;
