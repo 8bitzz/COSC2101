05a718700b46773920549d6af4b4843845640a76
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-07-31 08:13:50 +0700

    Add email password ref hook on Log In page

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
 import "./login.css";
+import { useRef, useState } from "react";
 
 const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = () => {
+    // Consume authen API
+  };
+
   return (
     <div className="login">
       <div className="navbar w-screen fixed top-0 z-50 text-white">
@@ -23,15 +34,41 @@ const LogIn = () => {
         </div>
       </div>
       <div className="container">
-        <form className="w-1/4 h-2/5 rounded-md bg-netflix-black flex flex-col justify-around p-4 opacity-80">
+        <form className="w-1/4 h-2/5 rounded-md bg-netflix-black flex flex-col justify-around p-6 opacity-80">
           <h1 className="text-xl font-semibold text-center">Sign In</h1>
-          <input className="h-12 rounded-md pl-2" type="email" placeholder="Email address"/>
-          <input className="h-12 rounded-md pl-2" type="password" placeholder="Password" />
-          <button className="bg-red-600 rounded-md py-2 px-4">Sign In</button>
-          <span>New to Netflix? <b><Link to="/signup">Sign Up Now</Link></b></span>
+          <input
+            className="h-12 rounded-md pl-2"
+            type="email"
+            placeholder="Email address"
+            ref={emailRef}
+            onChange={() => setEmail(emailRef.current.value)}
+          />
+          <input
+            className="h-12 rounded-md pl-2"
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+            onChange={() => setPassword(passwordRef.current.value)}
+          />
+          <button
+            className="bg-red-600 rounded-md py-2 px-4"
+            onClick={handleSubmit}
+          >
+            Sign In
+          </button>
+          <span>
+            New to Netflix?{" "}
+            <b>
+              <Link to="/signup">Sign Up Now</Link>
+            </b>
+          </span>
           <small>
             This page is protected by Google reCAPTCHA to ensure you're not a
-            bot. <b><a className="z-10 cursor-pointer">Learn more</a></b>.
+            bot.{" "}
+            <b>
+              <a className="z-10 cursor-pointer">Learn more</a>
+            </b>
+            .
           </small>
         </form>
       </div>
