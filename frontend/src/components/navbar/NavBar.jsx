8a9ef72b3bbad080257e34e1bf64c4b72318aa5b
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-07-31 09:13:26 +0700

    Refractor navbar

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,6 +1,116 @@
-import React from "react";
+import './navbar.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const NavBar = () => {
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("");
+  const [movies, setMovie] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [isFilled, setIsFilled] = useState(false);
+  const [isSet, setIsSet] = useState(false);
+  let history = useHistory();
+
+  //Fetch movie from API
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/movies`)
+      .then(res => {
+        setMovieList(res.data.data.movies.map((movie) => movie.category.name))
+        setMovie(res.data.data.movies.map(item => item.movies))
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    // eslint-disable-next-line
+  }, [])
+
+  // Function to handle when search field filled 
+  const handleSearch = (event) => {
+    setSearch(event.target.value); 
+    putSearchTerm(event.target.value);
+    event.preventDefault();
+    if (event.target.value === "") {
+      setIsFilled(false)
+    }
+    else {
+      setIsFilled(true)
+    }
+  }
+
+  // Handle change when genre selected 
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+    putCategoryTerm(event.target.value);
+    event.preventDefault();
+    if (event.target.value === "") {
+      isSet(false)
+    }
+    else {
+      setIsSet(true)
+    }
+  }
+
+  // Update URL when filter by genre
+  const putCategoryTerm = (value) => {
+    const params = new URLSearchParams();
+    console.log("putCategoryTerm", value)
+    if (value === "") {
+      history.push("")
+      return;
+    }
+    if (value) {
+      params.append("term", value)
+    }
+    else {
+      params.delete("term")
+    }
+    history.push({
+      pathname: "/genre",
+      search: params.toString()
+    })
+  }
+
+  // Update URL when search 
+  const putSearchTerm = (value) => {
+    const params = new URLSearchParams();
+    if (value === "") {
+      history.push("")
+      return;
+    }
+    if (value) {
+      params.append("term", value)
+    }
+    else {
+      params.delete("term")
+    }
+    history.push({
+      pathname: "/search",
+      search: params.toString()
+    })
+  }
+
+  const setSearchPath = () => {
+    if (isFilled === true) {
+      return "/search" 
+    }
+    else {
+      return "/" 
+    }
+  }
+
+  //Function to set path for Link at Genre selector
+  const setGenrePath = () => {
+    if (isSet === true) {
+      return "/genre" 
+    }
+    else {
+      return "/" 
+    }
+  }
+
   return (
     <div className="navbar w-screen fixed top-0 z-50 text-white">
       <div className="h-20 py-3 px-12 flex justify-between items-center text-sm">
