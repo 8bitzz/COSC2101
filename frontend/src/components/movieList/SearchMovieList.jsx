40bc0568698bb95075aa3a1fcdd40ab7f313b433
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-07-31 13:46:33 +0700

    Adjust search bar

--- frontend/src/components/movieList/SearchMovieList.jsx
+++ frontend/src/components/movieList/SearchMovieList.jsx
@@ -40,7 +40,7 @@ const SearchMovieList = () => {
             </div>
             <div className="grid md:grid-cols-4 lg:grid-cols-6 gap-4 ">
               {filteredMovies.map((movie, index) => {
-                return <MovieItem movie={movie} key={movie.id} index={index} />;
+                return <MovieItem movie={movie} key={movie._id} index={index} />;
               })}
             </div>
           </div>
