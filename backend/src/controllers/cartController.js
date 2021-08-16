bbcc5abfa478e669ad01b2bf469d8be0808e975d
Nghia Tran <vinhnghiatran@gmail.com>
2021-08-16 15:20:51 +0700

    Get cart logic

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -1,6 +1,18 @@
-const Cart = require("../models/cart")
-const catchAsync = require("../util/catchAsync")
+const Cart = require("../models/cart");
+const catchAsync = require("../util/catchAsync");
 
 // Async function to get all cart from a logged user
 exports.getAllCart = catchAsync(async (req, res, next) => {
+  // Get all cart by this user
+  const carts = await Cart.find({ createdBy: req.user.id })
+    .populate("movie")
+    .populate("createdBy");
+
+  // Return data
+  res.status(200).json({
+    status: "success",
+    data: {
+      carts,
+    },
+  });
 });
