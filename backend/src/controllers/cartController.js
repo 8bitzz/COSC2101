f6814f3034727f52c6e2947fdfbf3eb9d76505d8
Nghia Tran <vinhnghiatran@gmail.com>
2021-08-16 15:30:19 +0700

    Create a cart logic

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -16,3 +16,31 @@ exports.getAllCart = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+// Async function to create a new cart
+exports.createCart = catchAsync(async (req, res, next) => {
+  // Get movie id param
+  const movieID = req.query.movie_id;
+
+  // Create a cart
+  const cart = await Cart.create({
+    createdBy: req.user.id,
+    movie: movieID,
+  });
+
+  // If could not create a cart => error
+  if (!cart) {
+    return res.status(500).json({
+      status: "error",
+      message: "Could not create a cart",
+    });
+  }
+
+  // Return data
+  res.status(201).json({
+    status: "success",
+    data: {
+      cart,
+    },
+  });
+});
--- backend/src/routes/cartRoutes.js
+++ backend/src/routes/cartRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getAllCart
+  getAllCart,
+  createCart
 } = require("../controllers/cartController");
 const { isAuthenticated } = require("../controllers/userControllers");
 
 // Endpoint to get all cart from a loggin user
 router.get("/", isAuthenticated, getAllCart);
+router.post("/", isAuthenticated, createCart);
 
 module.exports = router;
