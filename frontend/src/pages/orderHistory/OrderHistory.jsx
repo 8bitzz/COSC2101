0a76146483e23a65f04425bfe722563146639270
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-08-24 11:22:27 +0700

    Styling order component

--- frontend/src/pages/orderHistory/OrderHistory.jsx
+++ frontend/src/pages/orderHistory/OrderHistory.jsx
@@ -5,6 +5,8 @@ import useSWR from "swr";
 import { useEffect, useState } from "react";
 import { BASE_API_URL } from "../../utils/constants";
 import { useHistory } from "react-router-dom";
+import Spinner from "../../components/spinner/Spinner";
+import ErrorPopover from "../../components/errorPopover/ErrorPopover";
 
 const orderFetcher = (url, token) =>
   axios
@@ -27,40 +29,15 @@ const OrderHistory = () => {
   );
 
   if (!data) {
-    return (
-      <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
-        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
-        <h2 className="text-center text-white text-xl font-semibold">
-          Loading...
-        </h2>
-        <p className="w-1/3 text-center text-white">
-          This may take a few seconds, please don't close this page.
-        </p>
-      </div>
-    );
-  }
-
-  if (error) {
-    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-5">
-      <span className="inline-block align-middle mr-8">
-        <b className="font-bold">Error!</b> {error}
-      </span>
-      <button
-        className="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none"
-        onClick={() => {
-          history.push("/");
-        }}
-      >
-        <span>×</span>
-      </button>
-    </div>;
+    return <Spinner />;
   }
 
   return (
     <div className="bg-netflix-black w-full h-full pt-24 text-white">
       <NavBar />
       <div className="h-screen w-full m-0 px-12">
-        <div className="">
+        {error && <ErrorPopover error={error} />}
+        <div className="justify-center mx-auto">
           <h1 className="text-white text-3xl font-semibold my-6">
             Order History
           </h1>
@@ -69,40 +46,57 @@ const OrderHistory = () => {
             Check the status of recent orders and your shipping status.
           </h2>
         </div>
-        <div className="mt-8">
-          <div className="flow-root lg:w-5/12">
-            <ul role="list" className="-my-6 divide-y divide-gray-900">
-              {data.orders[0].movies.map((movie) => (
-                <li key={movie._id} className="py-6 flex">
-                  <div className="flex-shrink-0 w-60 h-36 border-0 border-gray-200 rounded-md overflow-hidden">
-                    <img
-                      src={movie.thumbnail}
-                      alt={movie.title}
-                      className="w-full h-full object-center object-cover"
-                    />
-                  </div>
-
-                  <div className="ml-4 flex-1 flex flex-col">
-                    <div>
-                      <div className="flex justify-between text-xl font-bold text-gray-50">
-                        <h3>
-                          <a href={movie.trailerURL}>{movie.title}</a>
-                        </h3>
-                        <p className="ml-4 text-xl">$ {movie.price}</p>
+        <div className="mt-8 py-8 px-6 order-box w-full sm:w-full md:w-full lg:w-5/12">
+          <div className="flex flex-col">
+            <div className="flex flex-row justify-between mb-10">
+              <div>
+                <h1 className="text-lg font-medium text-gray-200">Order No.</h1>
+                <h2 className="text-gray-400 sm:truncate">{data.orders[0]._id}</h2>
+              </div>
+              <div>
+                <h1 className="text-lg font-medium text-gray-200">Date placed</h1>
+                <h2 className="text-gray-400">{data.orders[0].createdAt}</h2>
+              </div>
+              <div>
+                <h1 className="text-lg font-medium text-gray-200">Total amount</h1>
+                <h2 className="font-extrabold text-gray-300">$ {data.orders[0].amount}</h2>
+              </div>
+            </div>
+            <div className="flow-root">
+              <ul role="list" className="-my-6">
+                {data.orders[0].movies.map((movie) => (
+                  <li key={movie._id} className="py-6 flex">
+                    <div className="flex-shrink-0 w-60 h-36 border-0 border-gray-200 rounded-md overflow-hidden">
+                      <a href={movie.trailerURL}>
+                        <img
+                          src={movie.thumbnail}
+                          alt={movie.title}
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </a>
+                    </div>
+                    <div className="ml-4 flex-1 flex flex-col">
+                      <div>
+                        <div className="flex justify-between text-base text-gray-200">
+                          <h3 className="font-bold">{movie.title}</h3>
+                          <p className="ml-4 font-medium">$ {movie.price}</p>
+                        </div>
+                      </div>
+                      <div className="flex-1 flex flex-col justify-around text-sm">
+                        <div className="flex items-center mt-1 text-gray-400">
+                          <span>{movie.duration}</span>
+                          <span className="border border-gray-500 px-1 mx-2">
+                            16+
+                          </span>
+                          <span>{movie.publishYear}</span>
+                        </div>
+                        <div className="text-gray-200">{movie.description}</div>
                       </div>
                     </div>
-                    <div className="flex-1 flex flex-col justify-between text-sm">
-                      <div className="flex items-center my-2">
-                        <span>{movie.duration}</span>
-                        <span className="limit">{movie.duration}</span>
-                        <span>{movie.publishYear}</span>
-                      </div>
-                      <div className="desc">{movie.description}</div>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
