b9bd13950be07c42e8067ca665d64004804cffea
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-08-25 17:06:52 +0700

    Handler add item on Detail page using handler in App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,14 @@ import Login from "./pages/login/Login";
 import Details from "./components/movieList/Details";
 import SearchMovieList from "./components/movieList/SearchMovieList";
 import FilteredMovieList from "./components/movieList/FilteredMovieList";
-import AuthContext from './service/auth-context.js'
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import React, { Fragment} from "react";
+import AuthContext from "./service/auth-context.js";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import React, { Fragment } from "react";
 import Cart from "./pages/cart/Cart";
 import OrderHistory from "./pages/orderHistory/OrderHistory";
 import { BASE_API_URL } from "./utils/constants";
@@ -15,22 +20,22 @@ import axios from "axios";
 export default class App extends React.Component {
   //Set state for accessToken and _id
   state = {
-		accessToken: null,
-		_id: null,
-    count: 0
-	}
+    accessToken: null,
+    _id: null,
+    count: 0,
+  };
   //Login function to set current state of accessToken and _id
-	login = (accessToken, _id, tokenExpiration) => {
-		this.setState({ accessToken: accessToken, _id: _id })
-    localStorage.setItem('accessToken', this.state.accessToken)
-    localStorage.setItem('_id', this.state._id)
-	}
+  login = (accessToken, _id, tokenExpiration) => {
+    this.setState({ accessToken: accessToken, _id: _id });
+    localStorage.setItem("accessToken", this.state.accessToken);
+    localStorage.setItem("_id", this.state._id);
+  };
   //Logout function to return the null value of accessToken and _id
-	logout = () => {
-		this.setState({ accessToken: null, _id: null })
-    localStorage.removeItem('accessToken')
-    localStorage.removeItem('_id')
-	}
+  logout = () => {
+    this.setState({ accessToken: null, _id: null });
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("_id");
+  };
 
   handleItemRemove = (id) => {
     console.log("Item removed");
@@ -38,59 +43,85 @@ export default class App extends React.Component {
     // this.setState({count: this.state.count - 1});
     const token = localStorage.getItem("accessToken");
     axios
-    .delete(`${BASE_API_URL}/api/v1/carts?movie_id=${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-    .then((res) => this.setState({count: this.state.count - 1}))
-    .catch((err) => {
-      console.log(err);
-    });
-  }
+      .delete(`${BASE_API_URL}/api/v1/carts?movie_id=${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => this.setState({ count: this.state.count - 1 }))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   handleItemAdd = (id) => {
     console.log("Item added");
     console.log(id);
-    this.setState({count: this.state.count + 1});
-  }
+    // this.setState({ count: this.state.count + 1 });
+    const token = localStorage.getItem("accessToken");
+    const userID = localStorage.getItem("_id");
+    const data = {
+      movie: id,
+      createdBy: userID,
+    };
+    axios
+      .post(`${BASE_API_URL}/api/v1/carts?movie_id=${id}`, data, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => this.setState({ count: this.state.count + 1 }))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   componentDidMount() {
     const token = localStorage.getItem("accessToken");
     axios
-    .get(`${BASE_API_URL}/api/v1/carts`, {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-    .then((res) => this.setState({count: res.data.data.carts.length}))
-    .catch((err) => {
-      console.log(err);
-    });
+      .get(`${BASE_API_URL}/api/v1/carts`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => this.setState({ count: res.data.data.carts.length }))
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
-    console.log(this.state.count)
+    console.log(this.state.count);
     return (
       <div className="m-0">
         <Router>
           <Fragment>
-            <AuthContext.Provider value={{
-              accessToken: this.state.accessToken,
-              _id: this.state._id,
-              count: this.state.count,
-              login: this.login,
-              logout: this.logout,
-              handleItemRemove: this.handleItemRemove,
-              handleItemAdd: this.handleItemAdd
-            }} >
+            <AuthContext.Provider
+              value={{
+                accessToken: this.state.accessToken,
+                _id: this.state._id,
+                count: this.state.count,
+                login: this.login,
+                logout: this.logout,
+                handleItemRemove: this.handleItemRemove,
+                handleItemAdd: this.handleItemAdd,
+              }}
+            >
               <div className="bg-netflix-black">
                 <Switch>
                   <Route exact path="/" component={Home} />
-                  <Route path="/genre"><FilteredMovieList /></Route>
-                  <Route path="/search"><SearchMovieList /></Route>
-                  <Route path={`/details/:_id`} component={Details} ></Route>
-                  {localStorage.getItem('accessToken') && <Redirect from='/signup' to='/' exact />}
-                  <Route path="/signup" component={Signup} ></Route>
-                  {localStorage.getItem('accessToken') && <Redirect from='/login' to='/' exact />}
-                  <Route path="/login" component={Login} ></Route>
-                  {!localStorage.getItem('accessToken') && <Redirect from='/cart' to='/' exact />}
+                  <Route path="/genre">
+                    <FilteredMovieList />
+                  </Route>
+                  <Route path="/search">
+                    <SearchMovieList />
+                  </Route>
+                  <Route path={`/details/:_id`} component={Details}></Route>
+                  {localStorage.getItem("accessToken") && (
+                    <Redirect from="/signup" to="/" exact />
+                  )}
+                  <Route path="/signup" component={Signup}></Route>
+                  {localStorage.getItem("accessToken") && (
+                    <Redirect from="/login" to="/" exact />
+                  )}
+                  <Route path="/login" component={Login}></Route>
+                  {!localStorage.getItem("accessToken") && (
+                    <Redirect from="/cart" to="/" exact />
+                  )}
                   <Route path="/cart" component={Cart}></Route>
                   <Route path="/user/order" component={OrderHistory}></Route>
                 </Switch>
@@ -101,5 +132,4 @@ export default class App extends React.Component {
       </div>
     );
   }
-
 }
